496793eab25431a176d49ac33d25a67e50af6211
f3llps <fellipeversiani@gmail.com>
2018-04-21 20:27:34 -0300

    Adding a list of movies to layout test; Add MovieViewModel.

--- ArcTouch.UpcomingMovies/ArcTouch.UpcomingMovies/ViewModels/MainPageViewModel.cs
+++ ArcTouch.UpcomingMovies/ArcTouch.UpcomingMovies/ViewModels/MainPageViewModel.cs
@@ -3,6 +3,7 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using Xamarin.Forms;
@@ -11,11 +12,24 @@ namespace ArcTouch.UpcomingMovies.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService)
-            : base(navigationService)
+        private ObservableCollection<MovieViewModel> _movies = new ObservableCollection<MovieViewModel>();
+        public ObservableCollection<MovieViewModel> Movies
         {
-            Title = "Upcoming Movies - Main Page";
+            get { return _movies; }
+            set { SetProperty(ref _movies, value); }
         }
 
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            //Layout test
+            for (int i = 0; i < 10; i++)
+            Movies.Add(new MovieViewModel
+            {
+                Name = $"Movie {i}",
+                PosterImage = "https://image.tmdb.org/t/p/w500_and_h282_face/oVdLj5JVqNWGY0LEhBfHUuMrvWJ.jpg",
+                ReleaseDate = DateTime.Now,
+                Generes = "Action, Drama, (...)"
+            });    
+        }
     }
 }
---
+++ ArcTouch.UpcomingMovies/ArcTouch.UpcomingMovies/ViewModels/MovieViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+
+namespace ArcTouch.UpcomingMovies.ViewModels
+{
+    public class MovieViewModel : BindableBase
+    {
+        public MovieViewModel()
+        {
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private string _overview;
+        public string Overview
+        {
+            get { return _overview; }
+            set { SetProperty(ref _overview, value); }
+        }
+
+        private string _generes;
+        public string Generes
+        {
+            get { return _generes; }
+            set { SetProperty(ref _generes, value); }
+        }
+
+        private string _posterImage;
+        public string PosterImage
+        {
+            get { return _posterImage; }
+            set { SetProperty(ref _posterImage, value); }
+        }
+
+        private DateTime _releaseDate;
+        public DateTime ReleaseDate
+        {
+            get { return _releaseDate; }
+            set { SetProperty(ref _releaseDate, value); }
+        }
+    }
+}
